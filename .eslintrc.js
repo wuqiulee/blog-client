cadedbb06303ff0dde04a81a2be4b10f6638bfe2
wuqiuli <wutongte@163.com>
2022-12-03 17:34:22 +0800

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 0,
+    quotes: ['warn', 'single'],
+    'no-console': 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx', '.tsx', '.ts'],
+      },
+    ],
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-no-useless-fragment': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
